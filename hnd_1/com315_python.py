b0736468a4f86a77e276aff205481ec0ee3bf731
debaycisse <debaycisse@gmail.com>
2022-10-25 16:35:42 +0100

    work on the assignment

--- hnd_1/com315_python.py
+++ hnd_1/com315_python.py
@@ -1,75 +1,37 @@
-# This is the answer to a question, which is contained in com_315_assignment3.pdf file.
+from TowerHanoi import HanoiTower
 
-class TowerOfHanoi():
-    '''Move Disks from Peg 1 to Peg 2'''
-    def __init__(self, num_of_disk, peg1, peg2, peg3):
-        for i in range(1, self.num_of_disk+1):
-            peg1.append('disk{}'.format(i))
-        peg1.reverse()
-        
-        self.num_of_disk = num_of_disk
-        self.peg1 = peg1; self.peg2 = peg2; self.peg3 = peg3
+peg1 = peg2 = peg3 = []
+num_of_disk = 3
 
-    def move_disks(self):
-        # if self.num_of_disk <= 1:
-        #     self.peg2.append(self.peg1.pop())
-        #     print("1 -> 2")
-        #     return
-        # elif self.num_of_disk == 2:
-        #     self.peg3.append(self.peg1.pop())
-        #     print("1 -> 3")
-        #     self.peg2.append(self.peg1.pop())
-        #     print("1 -> 2")
-        #     self.peg2.append(self.peg3.pop())
-        #     print("3 -> 2")
-        #     return
-        # elif self.num_of_disk == 3:
-        #     self.peg3.append(self.peg1.pop())
-        #     print("1 -> 3")
+my_pegs = HanoiTower(num_of_disk, peg1, peg2, peg3)
 
+is_all_disk_in_peg2 = False
 
-        for disk in self.peg1:
-            if len(self.peg1) == self.num_of_disk and self.num_of_disk > 2:
-                while len(self.peg1) != 0:  # while peg1 is not empty.
-                    if len(self.peg2) == 0:
-                        self.peg2.append(self.peg1.pop())
-                        print("1 -> 2")
-                    elif len(self.peg3) == 0:
-                        self.peg3.append(self.peg1.pop())
-                        print("1 -> 3")
-                    elif self.peg3[len(self.peg3)-1] > self.peg2[len(self.peg2)-1]:
-                        self.peg3.append(self.peg2.pop())
-                        print("2 -> 3")
-                    elif len(self.peg1) == 1:
-                        self.peg2.append(self.peg1.pop())
-                        print("1 -> 2")    # peg 1 should be empty after this line here.
-                        # TODO checks if the remaining disk in peg3 is more than 2
+print("\n" + "-" * 20 + " Moving Hanoi Disk " + "-" * 20 + "\n")  # Display message
 
-                        # TODO check if the remaining disk in peg3 is exactly 2
-                        self.peg1.append(self.peg2.pop())
-                        print("2 -> 1")
-                        self.peg3.append(self.peg2.pop())
-                        print("2 -> 3")
-
-
-                    
-                    if len(self.peg2) == 0 and len(self.peg3) == 0:
-                        
-
-                    else:
-                        pass
-    
-            elif len(self.peg1) == self.num_of_disk and self.num_of_disk == 2:
-                self.peg3.append(self.peg1.pop())
-                print("1 -> 3")
-                self.peg2.append(self.peg1.pop())
-                print("1 -> 2")
-                self.peg2.append(self.peg3.pop())
-                print("3 -> 2")
-                return                
-            elif len(self.peg1) == self.num_of_disk and self.num_of_disk == 1:
-                self.peg2.append(self.peg1.pop())
-                print("1 -> 2")
-                return                
-
-
+while not is_all_disk_in_peg2:
+    if len(my_pegs.peg1) == 0 and len(my_pegs.peg3) == 0:
+        is_all_disk_in_peg2 = True
+    elif len(my_pegs.peg3) == 0:
+        my_pegs.move_1_to_3()
+        is_all_disk_in_peg2 = True      
+    elif len(my_pegs.peg2) == 0:
+        my_pegs.move_1_to_2()
+    elif len(my_pegs.peg1) == 0 and my_pegs.peg3[len(my_pegs.peg3)-1] > my_pegs.peg2[len(my_pegs.peg2)-1]:
+        my_pegs.move_2_to_3()
+    elif len(my_pegs.peg1) == 0 and my_pegs.peg2[len(my_pegs.peg2)-1] > my_pegs.peg3[len(my_pegs.peg3)-1]:
+        my_pegs.move_2_to_1()
+    elif len(my_pegs.peg2) == 0 and my_pegs.peg3[len(my_pegs.peg3)-1] < my_pegs.peg1[len(my_pegs.peg1)-1]:
+        my_pegs.move_3_to_1()
+    elif len(my_pegs.peg2) == 0 and my_pegs.peg3[len(my_pegs.peg3)-1] > my_pegs.peg1[len(my_pegs.peg1)-1]:
+        my_pegs.move_3_to_2()
+    elif len(my_pegs.peg3) == 0 and my_pegs.peg2[len(my_pegs.peg2)-1] > my_pegs.peg1[len(my_pegs.peg1)-1]:
+        my_pegs.move_1_to_3()
+    elif my_pegs.peg2[len(my_pegs.peg2)-1] > my_pegs.peg3[len(my_pegs.peg3)-1] and my_pegs.peg1[len(my_pegs.peg1)-1] > my_pegs.peg3[len(my_pegs.peg3)-1]:
+        my_pegs.move_1_to_2()
+    elif len(my_pegs.peg1) == 0 and my_pegs.peg3[len(my_pegs.peg3)-1] < my_pegs.peg2[len(my_pegs.peg2)-1]:
+        my_pegs.move_3_to_2()
+    elif my_pegs.peg2[len(my_pegs.peg2)-1] > my_pegs.peg3[len(my_pegs.peg3)-1]:
+        my_pegs.move_3_to_2()
+    elif my_pegs.peg3[len(my_pegs.peg3)-1] > my_pegs.peg2[len(my_pegs.peg2)-1]:
+        my_pegs.move_2_to_3()
